3e19bbdff0f9e66093fddacddda2e43e38e5eda4
shuichi-tomoyasu <shuichitmys@gmail.com>
2026-07-14 13:06:38 +0900

    update: MovingBoxの作成

--- src/test/MovingBox.java
+++ src/test/MovingBox.java
@@ -3,37 +3,30 @@ package test;
 import java.awt.Color;
 import java.awt.Graphics;
 
-import engine.graphics.Renderable;
-import engine.update.Updatable;
+import engine.object.GameObject;
 
-public class MovingBox implements Renderable, Updatable {
+public class MovingBox extends GameObject {
 
-    private double currentX;
-    private double currentY;
-    private double previousX;
     private int width;
     private int height;
 
-    public MovingBox(double currentX, double currentY, int width, int height) {
-        this.currentX = currentX;
-        this.currentY = currentY;
-        this.previousX = currentX;
+    public MovingBox(double x, double y, int width, int height) {
+        super(x, y);
         this.width = width;
         this.height = height;
     }
 
     @Override
     public void update() {
-        this.previousX = currentX;
-        this.currentX += 1.0;
+        setX(getX() + 1.0);
     }
 
     @Override
     public void draw(Graphics g, double alpha) {
-        //Lerp実装
-        double lerpX = (1 - alpha) * previousX + alpha * currentX;
+        // Lerp実装
+        double lerpX = lerp(getPreviousX(), getX(), alpha);
 
         g.setColor(Color.BLACK);
-        g.fillRect((int)lerpX, (int)this.currentY, this.width, this.height);
+        g.fillRect((int) lerpX, (int) getY(), this.width, this.height);
     }
 }
