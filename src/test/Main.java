501f1d4d02d2f69ac1f021d6288032c5227e7474
shuichi-tomoyasu <shuichitmys@gmail.com>
2026-07-15 12:23:36 +0900

    update: 画面表示だけにする

--- src/test/Main.java
+++ src/test/Main.java
@@ -17,16 +17,6 @@ public class Main {
         SwingUtilities.invokeLater(() -> {
             GameWindow window = new GameWindow(set);
             window.show();
-            GameRenderer renderer = new GameRenderer(window.getCanvas());
-            List<GameObject> r = new ArrayList<>();
-            List<GameObject> u = new ArrayList<>();
-            MovingBox box = new MovingBox(0, 200, 200, 300);
-            r.add(box);
-            u.add(box);
-
-            GameLoop gameLoop = new GameLoop(200, renderer, r, u);
-
-            gameLoop.start();
         });
     }
 }
