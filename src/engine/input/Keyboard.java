f7f7e726fd2a6d85475e22e33b0fc6be883d3405
shuichi-tomoyasu <shuichitmys@gmail.com>
2026-07-17 11:18:05 +0900

    update: pressed配列の宣言とkeyPressedとkeyReleasedの定義

--- src/engine/input/Keyboard.java
+++ src/engine/input/Keyboard.java
@@ -5,19 +5,28 @@ import java.awt.event.KeyListener;
 
 public class Keyboard implements KeyListener {
 
+    private static final int KEY_COUNT = 512;
+    private final boolean[] pressed = new boolean[KEY_COUNT];
+
     @Override
     public void keyTyped(KeyEvent e) {
-        System.out.println(e.getKeyChar());
+
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        System.out.println("Pressed");
+        int code = e.getKeyCode();
+        if (0 <= code && code < pressed.length) {
+            pressed[code] = true;
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        System.out.println("Released");
+        int code = e.getKeyCode();
+        if (0 <= code && code < pressed.length) {
+            pressed[code] = false;
+        }
     }
 
 }
