7852f4f0335b89eac6860b693cb0dcef69e9f8c9
shuichi-tomoyasu <shuichitmys@gmail.com>
2026-07-23 13:55:26 +0900

    update: MyGameの作成

---
+++ src/sample/MyGame.java
@@ -0,0 +1,34 @@
+package sample;
+
+import java.awt.event.KeyEvent;
+
+import engine.core.GameApplication;
+import engine.core.GameSettings;
+import engine.input.InputManager;
+
+public class MyGame extends GameApplication {
+    private int UPS = 120;
+
+    @Override
+    protected GameSettings createSettings() {
+        return new GameSettings(800, 600, "SampleGame", true, true, true, false);
+    }
+
+    @Override
+    protected void onInit() {
+        InputManager<Action> input = new InputManager<>(engine().getKeyboard(), Action.class);
+
+        input.addMapping(Action.MOVE_LEFT, KeyEvent.VK_LEFT);
+        input.addMapping(Action.MOVE_UP, KeyEvent.VK_UP);
+        input.addMapping(Action.MOVE_RIGHT, KeyEvent.VK_RIGHT);
+        input.addMapping(Action.MOVE_DOWN, KeyEvent.VK_DOWN);
+
+        engine().addObject(new Block(input, 200, 100, 3));
+    }
+
+    @Override
+    protected int targetUps() {
+        return this.UPS;
+    }
+
+}
