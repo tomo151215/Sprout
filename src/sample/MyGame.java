0a78dab71190b150477ae9e60efee6d849b7e62e
shuichi-tomoyasu <shuichitmys@gmail.com>
2026-07-29 16:17:59 +0900

    update:  テスト用ゲーム作成

--- src/sample/MyGame.java
+++ src/sample/MyGame.java
@@ -12,21 +12,14 @@ public class MyGame extends GameApplication {
     @Override
     protected GameSettings createSettings() {
         return GameSettings.builder()
-                .size(800,600)
+                .size(800, 600)
                 .title("SampleApp")
                 .build();
     }
 
     @Override
     protected void onInit() {
-        InputManager<Action> input = new InputManager<>(engine().getKeyboard(), Action.class);
-
-        input.addMapping(Action.MOVE_LEFT, KeyEvent.VK_LEFT);
-        input.addMapping(Action.MOVE_UP, KeyEvent.VK_UP);
-        input.addMapping(Action.MOVE_RIGHT, KeyEvent.VK_RIGHT);
-        input.addMapping(Action.MOVE_DOWN, KeyEvent.VK_DOWN);
-
-        engine().addObject(new Block(input, 200, 100, 3));
+        engine().addObject(new MousePointerBox(engine().getMouse()));
     }
 
     @Override
