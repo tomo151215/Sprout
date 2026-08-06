756660d134dab2ded175e73b99f6ba76bd8a16f4
shuichi-tomoyasu <shuichitmys@gmail.com>
2026-08-06 12:19:34 +0900

    clean: Graphicsのimport削除

--- src/engine/graphics/GameRenderer.java
+++ src/engine/graphics/GameRenderer.java
@@ -1,7 +1,6 @@
 package engine.graphics;
 
 import java.awt.Canvas;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferStrategy;
--- src/sample/MousePointerBox.java
+++ src/sample/MousePointerBox.java
@@ -1,7 +1,6 @@
 package sample;
 
 import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 import engine.input.Mouse;
