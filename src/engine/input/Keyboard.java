1b8565d1e42d82fda6abe07cb84e78ce69eaf400
shuichi-tomoyasu <shuichitmys@gmail.com>
2026-08-15 17:29:06 +0900

    hotfix: 範囲外の keyCode を渡した場合、ArrayIndexOutOfBoundsException が発生

--- src/engine/input/Keyboard.java
+++ src/engine/input/Keyboard.java
@@ -69,15 +69,21 @@ public class Keyboard implements KeyListener {
         return isWithinBounds(keyCode) && pressed[keyCode];
     }
 
-    private boolean isWithinBounds(int keyCode) {
-        return keyCode >= 0 && keyCode < KEY_COUNT;
-    }
-
     public boolean isJustPressed(int keyCode) {
-        return isPressed(keyCode) && !previousPressed[keyCode];
+        if (!isWithinBounds(keyCode)) {
+            return false;
+        }
+        return pressed[keyCode] && !previousPressed[keyCode];
     }
 
     public boolean isJustReleased(int keyCode) {
-        return !isPressed(keyCode) && previousPressed[keyCode];
+        if (!isWithinBounds(keyCode)) {
+            return false;
+        }
+        return !pressed[keyCode] && previousPressed[keyCode];
+    }
+
+    private boolean isWithinBounds(int keyCode) {
+        return keyCode >= 0 && keyCode < KEY_COUNT;
     }
 }
