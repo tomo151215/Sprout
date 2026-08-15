43da12747825d2fefc61fc4f3979cf126c836289
shuichi-tomoyasu <shuichitmys@gmail.com>
2026-08-15 14:00:41 +0900

    update: Spriteクラスの作成

---
+++ src/engine/asset/Sprite.java
@@ -0,0 +1,26 @@
+package engine.asset;
+
+import java.awt.image.BufferedImage;
+
+public class Sprite {
+    private final BufferedImage image;
+
+    public Sprite(BufferedImage image) {
+        if (image == null) {
+            throw new IllegalArgumentException("image must nut be null.");
+        }
+        this.image = image;
+    }
+
+    public int getWidth() {
+        return image.getWidth();
+    }
+
+    public int getHeight() {
+        return image.getWidth();
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+}
